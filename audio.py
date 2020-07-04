d9b6b813afcf83a4677fb9bcd6426f3a65378d80
Nevin1901 <nevinpuri1901@gmail.com>
2020-07-04 03:42:06 +0200

    made it so that the python script does everything

--- audio.py
+++ audio.py
@@ -3,12 +3,29 @@
 import sounddevice as sd
 import numpy as np
 
+audioData = []
+avgData = []
+
 duration = 2
 
 def print_sound(indata, outdata, frames, time, status):
     volume_norm = np.linalg.norm(indata)*10
-    print ("A" * int(volume_norm))
+    audioData.append("A" * int(volume_norm))
+ #   print ("A" * int(volume_norm))
+    
 
 with sd.Stream(callback=print_sound):
     sd.sleep(duration * 1000)
 
+
+for dataSample in audioData:
+    if len(dataSample) > 0:
+        avgData.append(len(dataSample))
+
+average = 0
+for filteredSample in avgData:
+    average += avgData[filteredSample]
+
+print(average / len(avgData))
+    
+    
